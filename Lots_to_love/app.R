7ecbb1ee5c238c4874be4f0657b0aa18c98af799
clambert15 <92833230+clambert15@users.noreply.github.com>
2022-02-01 20:13:08 -0500

    Added output of bar graph displaying projects by type (created a new dataframe based on the reactive as the foundation for the graph).

--- Lots_to_love/app.R
+++ Lots_to_love/app.R
@@ -33,19 +33,21 @@ ui <- fluidPage(
                                     "South Park", "Swissvale", "White Oak", "Wilkinsburg",
                                     "Unknown"))
         ),
+        
+        #fill by project status?
     
     mainPanel(
         tabsetPanel(
-        tabPanel("Data Table",DT::dataTableOutput(outputId = "table"))  
-        #tabPanel("Summary", verbatimTextOutput("summary")), 
-        ),
+        tabPanel("Project Types", plotOutput(outputId = "type_graph")) 
         #DT::dataTableOutput(outputId = "lotstable"),
         #plot1: lots by type
         #plot2: projects implemented over time
         #plot3: projects by project stage
         
-    )
+   ),
+   dataTableOutput(outputId = "table")
   )
+ )
 )
 
 # Define server logic required to draw a histogram
@@ -53,12 +55,25 @@ server <- function(input, output) {
 
 #Create subset
     lots_subset <- reactive({
-        #req(input$filter) # ensure availablity of value before proceeding
+        req(input$filter) # ensure availablity of value before proceeding
         filter(lots, location %in% input$filter)
     })
     
+
+    output$type_graph <- renderPlot({
+        by_location <-
+            lots_subset() %>%
+                group_by(type) %>%
+                summarize(count = n())
+        
+        ggplot(data = by_location, aes(x = type, y = count)) +
+        geom_bar(stat = "identity") +
+        theme(axis.text.x = element_text(angle = 45, hjust=1))
+    })
+    
+    
     output$table <- DT::renderDataTable({
-        DT::datatable(data = lots_subset(),
+        DT::datatable(data = lots_subset()[,2:5],
                       rownames = FALSE)
     
     }
