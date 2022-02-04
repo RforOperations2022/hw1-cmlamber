69caa6774fd24898c34678cf101b49913a548e94
clambert15 <92833230+clambert15@users.noreply.github.com>
2022-02-03 22:20:46 -0500

    Added an input to change the x axis on one of the graphs. Added line graph

--- Lots_to_love/app.R
+++ Lots_to_love/app.R
@@ -3,8 +3,11 @@ library(ggplot2)
 library(DT)
 library(shinythemes)
 library(dplyr)
+library(stringr)
+library(tools)
 
 lots <- read.csv("lots_to_love_cleaned.csv")
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -15,9 +18,11 @@ ui <- fluidPage(
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-      selectInput(inputId = "fill_by",
-                         label = "Fill histograms by:",
-                         choices = c("No fill", "Location")),
+
+     selectInput("x", "Choose a variable for the x:",
+                  choices = c("Location" = "location",
+                              "Project type" = "type")),
+      
       hr(),
       
       checkboxInput(inputId = "display_locations",
@@ -58,8 +63,10 @@ ui <- fluidPage(
     
     mainPanel(
       tabsetPanel(
+        tabPanel("Flex graph", plotOutput(outputId = "flex_graph")),
         tabPanel("Project Types", plotOutput(outputId = "type_graph")),
-        tabPanel("Tables",dataTableOutput(outputId = "table"))
+        tabPanel("Over Time", plotOutput(outputId = "line_plot")),
+        tabPanel("Tables", dataTableOutput(outputId = "table"))
         #DT::dataTableOutput(outputId = "lotstable"),
         #plot2: projects implemented over time
         #plot3: projects by project stage
@@ -73,32 +80,58 @@ server <- function(input, output) {
   
   #Create subset
   lots_subset <- reactive({
-    #req(input$filter) # ensure availablity of value before proceeding
-    filter(lots, location %in% input$filter)
+    req(input$filter) # ensure availablity of value before proceeding
+    filter(lots, location %in% input$filter) })
+  
+  aggregated <- reactive({
+      lots %>%
+      group_by(across(all_of(input$x))) %>%
+      summarize(count = n())
+  })
+
+  output$flex_graph <- renderPlot({
+    ggplot(data = aggregated(), aes(y = count)) +
+      geom_bar(stat = "identity", aes_string(x = input$x)) +
+      theme(axis.text.x = element_text(angle = 45, hjust=1))
   })
   
   
-  output$type_graph <- renderPlot({
-    by_location <-
+  
+  output$line_plot <- renderPlot({
+    by_year <-
       lots_subset() %>%
-      group_by(type) %>%
+      subset(year_implemented != "") %>%
+      group_by(year_implemented) %>%
       summarize(count = n())
     
-    ggplot(data = by_location, aes(x = type, y = count)) +
+    ggplot(data = by_year, aes(x = year_implemented, y = count)) +
+    geom_point() +
+    geom_line() +
+    theme(axis.text.x = element_text(angle = 45, hjust=1)) +
+    labs(x = "Year implemented", y = "Number of projects implemented")
+  })
+  
+  output$type_graph <- renderPlot({
+    by_type <-
+      lots_subset() %>%
+      group_by(type) %>%
+      summarize(count = n()) 
+    
+    ggplot(data = by_type, aes(x = type, y = count)) +
       geom_bar(stat = "identity") +
       theme(axis.text.x = element_text(angle = 45, hjust=1))
   })
   
   
   output$table <- DT::renderDataTable({
-    DT::datatable(data = lots_subset()[,2:5],
+    DT::datatable(data = lots_subset(),
                   rownames = FALSE)
     
   })
   
   output$download <- downloadHandler(
        filename = function() {
-         paste('data-', Sys.Date(), '.csv', sep='')
+         paste('lots_to_love_data-', Sys.Date(), '.csv', sep='')
        },
        content = function(con) {
          write.csv(lots_subset(), con)
